0e36b3486116605678f7c58306edb37c12ced9bc
Marcos Alejandro <cypherchabon@gmail.com>
2023-02-25 18:49:54 -0300

    feat: Define and implement EllipticCurve

---
+++ src/elliptic_curve.rs
@@ -0,0 +1,56 @@
+/// Elliptic curve
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+pub struct EllipticCurve {
+    /// The coefficient of the x term
+    a: u32,
+    /// The constant term
+    b: u32,
+}
+
+impl EllipticCurve {
+    /// Create a new elliptic curve.
+    /// Arguments:
+    /// * `a`: the coefficient of the x term
+    /// * `b`: the constant term
+    pub fn new(a: u32, b: u32) -> Self {
+        Self { a, b }
+    }
+}
+
+impl std::fmt::Display for EllipticCurve {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "y^2 = x^3 + {}x + {}", self.a, self.b)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_new() {
+        let curve = EllipticCurve::new(1, 2);
+        assert_eq!(curve.a, 1);
+        assert_eq!(curve.b, 2);
+    }
+
+    #[test]
+    fn test_fmt() {
+        let curve = EllipticCurve::new(1, 2);
+        assert_eq!(format!("{}", curve), "y^2 = x^3 + 1x + 2");
+    }
+
+    #[test]
+    fn test_eq() {
+        let curve1 = EllipticCurve::new(1, 2);
+        let curve2 = EllipticCurve::new(1, 2);
+        assert_eq!(curve1, curve2);
+    }
+
+    #[test]
+    fn test_ne() {
+        let curve1 = EllipticCurve::new(1, 2);
+        let curve2 = EllipticCurve::new(2, 1);
+        assert_ne!(curve1, curve2);
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,3 @@
+pub mod elliptic_curve;
 pub mod finite_field;
 pub mod finite_field_element;
